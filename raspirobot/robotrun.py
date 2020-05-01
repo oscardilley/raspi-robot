fecc0830e442885fe8712cf1c7a4bf73a742672f
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-05-01 16:49:55 +0100

    Update robotrun.py

--- raspirobot/robotrun.py
+++ raspirobot/robotrun.py
@@ -1,9 +1,1 @@
 print("Hello, welcome to my robot") #test
-from picamera import PiCamera
-from picamera import color
-from time import sleep
-
-camera = PiCamera()
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
