ccc03e5a82ef9e02623c81564317cfd5c3785be3
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-05-01 16:57:04 +0100

    Update robotrun.py

--- raspirobot/robotrun.py
+++ raspirobot/robotrun.py
@@ -1,1 +1,10 @@
-print("Hello, welcome to my robot") #test
+#-------------------------------------------------------------------------------------------------------------------------
+# This program is run from __main__ on run of this repository and this file functions as main centre to run this project.
+# This will be responsible for all the necessary imports of the camera, sensors and motor software to run the robot.
+# It will also initalise the GUI
+# This should be clearly organised and simple, calling functions without much other code.
+#-------------------------------------------------------------------------------------------------------------------------
+
+From time import sleep
+print("Initialising robot...")
+sleep(3)
