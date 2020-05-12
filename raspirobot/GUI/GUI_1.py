89b6878c4ae958b58890ec70076b9aca078faa70
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-05-12 17:36:21 +0100

    Update GUI_1.py

--- raspirobot/GUI/GUI_1.py
+++ raspirobot/GUI/GUI_1.py
@@ -3,3 +3,20 @@ from guizero import MenuBar
 from time import sleep
 
 def gui_1():
+  main_screen = App(title"Welcome to my Robot")
+  sleep(2)
+  def function_1():
+    main_screen.full_screen = True
+    return
+  def function_2():
+    home_page.full_screen = False
+    return
+  
+  menu = MenuBar(main_screen,
+                 toplevel=["File"],
+                 options=[
+                   [["Fullscreen", function_1], ["Exit Fullscreen", function_2]]
+                 ])
+  return
+
+gui_1()
