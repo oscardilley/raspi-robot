6d9319654782aef37ec1465557fdfd64d1e5d35c
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-05-12 18:00:13 +0100

    Update robotrun.py

--- raspirobot/robotrun.py
+++ raspirobot/robotrun.py
@@ -3,37 +3,29 @@
 # This will be responsible for all the necessary imports of the camera, sensors and motor software to run the robot.
 # It will also initalise the GUI
 # This should be clearly organised and simple, calling functions without much other code.
+# gui program should manage most of the mess, camera function should allow for changing display
+# gui needs to return button press information and pwm slider information
+# gui must have button exlcusivity
+# use a text.repeat(500, distance_average) to call the distance into GUI every 0.5s
+
+# 1. run import statements
+# 2. call average distance function
+# 3. call camera
+# 4. call gui
+
 #-------------------------------------------------------------------------------------------------------------------------
 
 from time import sleep
-#boot up initial functionality and rest for a second or two
-print("Initialising robot...")
-sleep(2)
-
-#import distance functions, find a way to use a function to calculate the average value using def distance_average(1, 2):
-
 from sensor.distance_1 import distance_1
+from GUI.GUI_1 import gui_1
 
-#boot up initial functionality and rest for a second or two
+sleep(5)
+gui_1()
+sleep(5)
+
 while True:
   dist = distance_1()
   print("Distance = %.2f" %dist)
-  sleep(0.5)
-#import forwards, backwards, left, right functions
+  sleep(1)
 
-#run camera function
-
-#link camera display/ preview function to a show camera button on centre of GUI
-
-#change view button on GUI cycles through desired PI camera effects
-
-#use a text.repeat(500, distance_average) to call the distance into GUI every 0.5s
-
-#take an input value from slider 0-100 for %PWM speed and input that into all direction functions
-
-#link the direction functions to the buttons, on when held
-
-#make buttons exclusive to press if possible
-
-#final GUI.display()
 
