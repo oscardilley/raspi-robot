6fbc2742ba958c6bc4862a11050edc0c900cda5f
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-05-12 17:12:00 +0100

    Update distance_1.py

--- raspirobot/sensor/distance_1.py
+++ raspirobot/sensor/distance_1.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 from time import sleep
 from time import time
 GPIO.setmode(GPIO.BOARD)
-trigger_1 = 12 #using pi board numbering, select the sensor 1 gpio trigger
-echo_1 = 13 #select the echo pin for the input
+trigger_1 = 13 #using pi board numbering, select the sensor 1 gpio trigger
+echo_1 = 12 #select the echo pin for the input
 GPIO.setup(trigger_1, GPIO.OUT)
 GPIO.setup(echo_1, GPIO.IN)
 GPIO.output(trigger_1, False)
