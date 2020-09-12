585f18e02a376c61c066351a39eb8cccde65919a
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-09-12 11:56:33 +0100

    Update distance_test_2.py

--- raspirobot/testing/distance_test_2.py
+++ raspirobot/testing/distance_test_2.py
@@ -1,6 +1,7 @@
 # this file is setup to test the functionality of my distance sensing program and to experiment with how to read off the distance
 
-from ...sensor import distance_1
+import raspirobot.sensor
+from raspirobot.sensor import distance_1
 
 while True:
   distance = distance_1()
