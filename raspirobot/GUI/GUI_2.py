359fe31ebc7b0900e239edc71745406644d76df5
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-09-16 11:14:29 +0100

    Update GUI_2.py

--- raspirobot/GUI/GUI_2.py
+++ raspirobot/GUI/GUI_2.py
@@ -1,2 +1,32 @@
 # second attempt at a robot GUI, trying to get the camera preview to import
 
+from guizero import App
+from guizero import MenuBar
+from time import sleep
+from picamera import PiCamera
+
+def gui_2():
+  main_screen = App(title="Raspi-Robot")
+  def function_1():
+    main_screen.full_screen = True
+    return
+  def function_2():
+    main_screen.full_screen = False
+    return
+  
+  menu = MenuBar(main_screen,
+                 toplevel=["File"],
+                 options=[
+                   [["Fullscreen", function_1], ["Exit Fullscreen", function_2]]
+                 ])
+  
+  camera = PiCamera()
+  camera.start_preview()
+  sleep(5)
+  camera.stop_preview()
+  
+  main_screen.display()
+  return
+
+gui_2()
+
