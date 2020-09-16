1fbec190ed8ed6f3d0299eadd54e605f2f0dbf58
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-09-16 12:32:10 +0100

    Update camera_preview_1.py

--- raspirobot/camera/camera_preview_1.py
+++ raspirobot/camera/camera_preview_1.py
@@ -3,7 +3,7 @@ from time import sleep
 
 camera = PiCamera()
 camera.start_preview()
-camera. resolution = (100, 70)
+camera. resolution = (960, 540)
 sleep(5)
 # camera.image_effect = "colorswap"
 # camera.image_effect = "negative"
