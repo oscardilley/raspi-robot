3ac1e28fe9f5eb74c2cfafe3c64d6e9ee24af4a4
oscardilley <62892646+oscardilley@users.noreply.github.com>
2020-12-22 16:43:50 +0000

    Update motion_functions.py

--- raspirobot/motion/motion_functions.py
+++ raspirobot/motion/motion_functions.py
@@ -1,21 +1,38 @@
 from time import sleep
 import RPi.gpio as gpio
 
-gpio.setmode(gpio.BOARD)
-gpio.clearup()
-enable1 = 3
-enable2 = 11
-input1 = 5
-input2 = 7
-input3 = 8
-input4 = 10
-gpio.setup(enable1, gpio.OUT)
-gpio.setup(enable2, gpio.OUT)
-gpio.setup(input1, gpio.OUT)
-gpio.setup(input2, gpio.OUT)
-gpio.setup(input3, gpio.OUT)
-gpio.setup(input4, gpio.OUT)
-pwm1 = gpio.PWM(enable1, 100)
-pwm2 = gpio.PWM(enable2, 100)
 
-def forward_move():
+class RaspiMotion():
+  
+  def __init__(self):
+    gpio.setmode(gpio.BOARD)
+    self.en1 = 3
+    self.en2 = 11
+    self.in1 = 5
+    self.in2 = 7
+    self.in3 = 8
+    self.in4 = 10
+    gpio.setup(en1, gpio.OUT)
+    gpio.setup(en2, gpio.OUT)
+    gpio.setup(in1, gpio.OUT)
+    gpio.setup(in2, gpio.OUT)
+    gpio.setup(in3, gpio.OUT)
+    gpio.setup(in4, gpio.OUT)
+    pwm1 = gpio.PWM(en1, 100)
+    pwm2 = gpio.PWM(en2, 100)
+
+
+def forward_move(self):
+  gpio.output(self.in1 , False)
+  gpio.output(self.in2 , True)
+  gpio.output(self.in3 , True)
+  gpio.output(self.in4 , False)
+  self.pwm1.ChangeDutyCycle(50)
+  self.pwm2.ChangeDutyCycle(50) 
+  gpio.output(self.en1, True)
+  gpio.output(self.en2, True)
+  sleep(5)
+  gpio.output(self.en1, False)
+  gpio.output(self.en2, False)
+  
+RaspiMotion.forward_move()
