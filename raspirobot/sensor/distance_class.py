5512956700543ad1064f68fe5993f858815aaf86
Oscar Dilley <62892646+oscardilley@users.noreply.github.com>
2020-12-26 14:10:41 +0000

    Create distance_class.py

---
+++ raspirobot/sensor/distance_class.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as gpio
+from time import sleep, time
+
+class Distance_Sensor():
+
+  def __init__(self):
+    self.trigger = 13
+    self.echo = 12
+    gpio.setmode(gpio.BOARD)
+    gpio.setmode(self.trigger, gpio.OUT)
+    gpio.setmode(self.echo, gpio.IN)
+    GPIO.output(self.trigger, False)
+  
+  def distance_function(self)
+    GPIO.output(self.trigger, True)
+    sleep(0.00001)
+    GPIO.output(self.trigger, False)
+    pulse_start = time()
+    pulse_end = time()
+    while gpio.input(self.echo) == 0:
+      pulse_start = time()
+    while gpio.input(self.echo) == 1:
+      pulse_end = time()
+    pulse_duration = pulse_end - pulse_end
+    distance = pulse_duration*(34326/2)
+    return distance
+ 
+  def returning_distance(self):
+    while True:
+        sleep(1)
+        print(self.distance_function())
+    
+test = Distance_Sensor()
+test.returning_distance()
+
+
